7e83d6c67a3a0155f02f22bcdf06fda6f573a40b
Jobin George <jobintweets@gmail.com>
2020-06-24 12:34:50 +0530

    All rendering methods tried out

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ import * as serviceWorker from './serviceWorker';
 
 function LoginButton(props) {
   return (
+    <div>
+    <p>You are logged out</p>
     <button onClick={props.onClick}>
       Login
     </button>
-  );
+    </div>
+    );
 }
 
 function LogoutButton(props) {
   return (
+    <div>
+    <p>Your are logged in.</p>
     <button onClick={props.onClick}>
       Logout
     </button>
+    </div>
+    
   );
 }
 class LoginControl extends React.Component {
@@ -35,23 +42,82 @@ class LoginControl extends React.Component {
 
   render () {
     const isLoggedIn = this.state.isLoggedIn;
-    let button;
+    let result;
     if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
+      result = <LogoutButton onClick={this.handleLogoutClick} />;
     } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
+      result = <LoginButton onClick={this.handleLoginClick} />;
     }
     return (
       <div>
       <h1>Welcome to my application</h1>
-       {button}
+       {result}
       </div>
     );
   }
 }
 
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  unreadMessages.forEach(element => {
+    console.log(element);
+  });
+  return (
+    <div>
+    
+      <h1>Hello!</h1>
+      {/* Therefore, if the condition is true, the element right after && will appear in the output */}
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React','Jobin'];
 ReactDOM.render(
-  <LoginControl />,
+  // <LoginControl />,
+  // <Mailbox unreadMessages={messages} />,
+  <Page/>,
   document.getElementById('root')
 );
 serviceWorker.register();
