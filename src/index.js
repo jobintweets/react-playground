bf2dc0fde0ca1d43ebd8ade7cd5acfda4b223c40
Jobin George <jobintweets@gmail.com>
2020-06-24 11:26:57 +0530

    Event handling

--- src/index.js
+++ src/index.js
@@ -3,82 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  )
-}
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">{props.user.name}</div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-    <UserInfo user={props.author}/>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {props.date.toLocaleDateString()}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: "I hope you enjoy learning React!",
-  author: {
-    name: "Hello Kitty",
-    avatarUrl: "https://placekitten.com/g/64/64"
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
   }
-};
+return (
+    <a href="https://codepen.io/gaearon/pen/vXdGmd?editors=0010" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
 
-class  Clock extends React.Component  {
+class Toggle extends React.Component {
   constructor(props) {
-super(props);
-this.state = {
-  date:new Date()
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(state => ({ isToggleOn: !state.isToggleOn}));
+  }
+render () {
+  return (
+    <button onClick={this.handleClick}>
+    {this.state.isToggleOn ? 'ON' : 'OFF'}
+  </button>
+);
 }
-  }
-  //life cycle method
-  /***
-   * The componentDidMount() method runs after 
-   * the component output has been rendered to the DOM. 
-   */
-  componentDidMount() {
-    setInterval( () => this.tick(), 1000 );
-  }
-// life cycle method
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-tick() {
-  this.setState({ date: new Date()});
 }
-  render () {
-    return (
-      <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-    </div>
-    );
-  }
-  }
-
+ 
+  
 ReactDOM.render(
-<Clock/>,
+<Toggle/>,
   document.getElementById('root')
 );
 
