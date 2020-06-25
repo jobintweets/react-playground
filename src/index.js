2fd8578a54f0adab07f65327cf8271f24b3fc7ac
Jobin George <jobintweets@gmail.com>
2020-06-25 10:14:02 +0530

    Lists and keys implemented

--- src/index.js
+++ src/index.js
@@ -3,121 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function LoginButton(props) {
-  return (
-    <div>
-    <p>You are logged out</p>
-    <button onClick={props.onClick}>
-      Login
-    </button>
-    </div>
-    );
-}
-
-function LogoutButton(props) {
-  return (
-    <div>
-    <p>Your are logged in.</p>
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-    </div>
-    
+const numbers = [1, 2, 3, 4, 5];
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number*2}</li>
   );
-}
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render () {
-    const isLoggedIn = this.state.isLoggedIn;
-    let result;
-    if (isLoggedIn) {
-      result = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      result = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    return (
-      <div>
-      <h1>Welcome to my application</h1>
-       {result}
-      </div>
-    );
-  }
-}
-
-function Mailbox(props) {
-  const unreadMessages = props.unreadMessages;
-  unreadMessages.forEach(element => {
-    console.log(element);
-  });
+  console.log(listItems);
   return (
-    <div>
-    
-      <h1>Hello!</h1>
-      {/* Therefore, if the condition is true, the element right after && will appear in the output */}
-      {unreadMessages.length > 0 &&
-        <h2>
-          You have {unreadMessages.length} unread messages.
-        </h2>
-      }
-    </div>
+    <ul>{listItems}</ul>
   );
 }
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
-  return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
-
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {showWarning: true};
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
-}
-
-const messages = ['React', 'Re: React', 'Re:Re: React','Jobin'];
 ReactDOM.render(
-  // <LoginControl />,
-  // <Mailbox unreadMessages={messages} />,
-  <Page/>,
+  <NumberList numbers= {numbers}/>,
   document.getElementById('root')
 );
 serviceWorker.register();
