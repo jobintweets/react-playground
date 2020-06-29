fa6d664b15d684bf6b27d09486949063dc58979b
Jobin George <jobintweets@gmail.com>
2020-06-29 12:41:55 +0530

    Passing componets as props

--- src/index.js
+++ src/index.js
@@ -5,101 +5,60 @@ import './index.css';
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
+function Contacts() {
+  return(
+  <div className="Contacts">
+  <h1>Welocome to Contacts component!!!1</h1>
+  </div>
+  ); 
 }
 
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
+function Chat() {
+  return (
+  <div className="Chat" >
+  <h1>Welcome to chat component!!1</h1>
+  </div>
+  );
 }
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
-}
-//child component.
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    
-  }
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);;
-  }
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
-               onChange={this.handleChange} />
-      </fieldset>
-    );
-  }
-}
-//parent component
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = {
-      temperature: '',
-       scale: 'c'};
-  }
-  handleCelsiusChange(temperature) {
-    this.setState({scale: 'c', temperature});
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({scale: 'f', temperature});
-  }
-    render() {
-      const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-      return (
-       <div>
-         <TemperatureInput
-          scale="c"
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange} />
-        <TemperatureInput
-          scale="f"
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange} />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)} />
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
       </div>
-       
-      );
-    }
-  }
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
 
-  const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-  };
-
+  function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+    <h2>Fancy Border!!</h2>
+      {props.children}
+    </div>
+  );
+}
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+      
+    </FancyBorder>
+  );
+}
 ReactDOM.render(
-//   <BrowserRouter>
-//   <App />
-// </BrowserRouter>,
-<Calculator/>,
+  <SplitPane
+    left={ <Contacts />}
+    right = {<Chat/>}
+  />,
   document.getElementById('root')
 );
 serviceWorker.register();
